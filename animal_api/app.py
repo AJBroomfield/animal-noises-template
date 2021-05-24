4b61e56a474bb28e9803137025647f88ab635d55
Andrew <ajbroomfield94@gmail.com>
2021-05-24 17:19:17 +0000

    basic API functionality

--- animal_api/app.py
+++ animal_api/app.py
@@ -1,24 +1,19 @@
-from flask import Flask, Response
-from random import randint
+from flask import Flask, request
+import random
 
 app = Flask(__name__)
 
+
 # animal generator route here
-@app.route('/animal', methods=['GET'])
-def animal():
-    animals = ["pig","cow","horse"]
-    animal = animals[randint(0,len(animals)-1)]
-    return animal
+@app.route('/get_animal', methods=['GET'])
+def get_animal():
+    return random.choice(['cow','pig','horse'])
 
 # animal noise generator route here
-@app.route('/noise/<string:key>', methods=['POST'])
-def noise(key):
-    noise = {"pig":"oink","cow":"moo","horse":"neigh"}
-    if key not in noise:
-        return Response(key +" currently not defined")
-    else:
-        return Response("The "+ key + " goes "+f'{noise[key]}')
-
+@app.route('/get_noise', methods=['POST'])
+def get_noise():
+    noises = {"pig":"oink","cow":"moo","horse":"neigh"}
+    return noises[request.data.decode('utf-8')]
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
